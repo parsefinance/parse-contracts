fd6f5127fb46b5593084d9160494c12003e9ea1c
m-fatehi <m.r.fatehinia@gmail.com>
2022-10-03 17:49:21 +0330

    feat: contracts for mocking ParseToken and Oracle

---
+++ contracts/mocks/Mock.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.0;
+
+contract Mock {
+    event FunctionCalled(
+        string instanceName,
+        string functionName,
+        address caller
+    );
+    event FunctionArguments(uint256[] uintVals, int256[] intVals);
+    event ReturnValueInt256(int256 val);
+    event ReturnValueUInt256(uint256 val);
+}
