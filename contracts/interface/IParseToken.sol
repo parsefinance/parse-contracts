002d9aab137b1ca436ec4ab6c5b9b08b0f0fb795
m-fatehi <m.r.fatehinia@gmail.com>
2022-10-03 17:45:40 +0330

    feat: interfaces

---
+++ contracts/interface/IParseToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+interface IParseToken {
+    function totalSupply() external view returns (uint256);
+
+    function rebase(uint256 epoch, int256 supplyDelta)
+        external
+        returns (uint256);
+
+    function setTaxRate(uint256 taxRate) external;
+
+    function DECIMALS() external returns (uint256);
+}
