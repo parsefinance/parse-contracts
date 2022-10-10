3a62aa41e1411fa830e6a2232805b1f47c283629
Amir <amir@parsefinance.org>
2022-10-10 13:49:20 +0330

    ci: add mumbai network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,18 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import '@nomiclabs/hardhat-ethers'
 import '@openzeppelin/hardhat-upgrades'
+import dotenv from "dotenv"
+dotenv.config()
 
 const config: HardhatUserConfig = {
   solidity: "0.8.16",
+  networks: {
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.MUMBAI_PRIVATE_KEY!]
+    }
+  },
+
 };
 
 export default config;
